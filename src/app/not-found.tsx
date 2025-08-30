53007ae42e09c58d2b177c43eb1356547da69651
Said Behzadmanesh <behmanesh_s@yahoo.com>
2025-08-31 01:10:22 +0200

    not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export const metadata = {
+  title: "Page Not Found",
+};
+
+export default function NotFound() {
+  return (
+    <div className="px-2 w-full">
+      <div className="mx-auto py-4 flex flex-col justify-center items-center gap-4">
+        <h2 className="text-2xl">Page Not Found</h2>
+        <Image
+          className="m-0 rounded-xl"
+          src="/images/not-found.jpg"
+          alt="page not found"
+          width={500}
+          height={500}
+          priority={true}
+          title="page not found"
+        />
+      </div>
+    </div>
+  );
+}
