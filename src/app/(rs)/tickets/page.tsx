ea9921ff08cee6bf4df240b0c17fe232f849ba3b
Said Behzadmanesh <behmanesh_s@yahoo.com>
2025-08-30 11:58:28 +0200

    initial structure

---
+++ src/app/(rs)/tickets/page.tsx
@@ -0,0 +1,3 @@
+export default function tickets() {
+  return <h2>Tickets page</h2>;
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { File, HomeIcon, UsersRound } from "lucide-react";
+import NavButton from "./NavButton";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export function Header() {
+  const pathname = usePathname();
+
+  return (
+    <header
+      key={pathname}
+      className="animate-slide bg-background h-12 p-2 border-b sticky top-0 z-20"
+    >
+      <div className="flex h-8 items-center justify-between w-full">
+        <div className="flex items-center gap-2">
+          <NavButton icon={HomeIcon} label="Home" href="/home" />
+          <Link
+            href="/home"
+            className="flex justify-center items-center gap-2 ml-0"
+            title="Home"
+          >
+            <h1 className="hidden sm:block text-xl font-bold m-0 mt-1">
+              Computer repair shop
+            </h1>
+          </Link>
+        </div>
+        <div className="flex items-center">
+          <NavButton icon={File} label="Tickets" href="/tickets" />
+          <NavButton icon={UsersRound} label="Customers" href="/customers" />
+        </div>
+      </div>
+    </header>
+  );
+}
